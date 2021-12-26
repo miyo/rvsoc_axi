07a2b937d7b8fcb81007b1efe1b38032c8488283
Takefumi MIYOSHI <miyo@wasamon.net>
2021-12-26 20:00:42 +0900

    add the project for Arty-100T and built files

---
+++ arty100t.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2.1 (lin64) Build 2729669 Thu Dec  5 04:48:12 MST 2019
+-- Date        : Sun Dec 26 19:46:13 2021
+-- Host        : tama running 64-bit Ubuntu 20.04.3 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/miyo/Desktop/rvsoc/rvsoc_src_ver053_axi/arty100t.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1_stub.vhdl
+-- Design      : clk_wiz_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_1 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_1;
+
+architecture stub of clk_wiz_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,resetn,locked,clk_in1";
+begin
+end;
